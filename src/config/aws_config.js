dc86706f10cf2bb6881b8f7c1acfeb6fb04deb2e
alokpatel <alokpatel863@daffodilsw.com>
2022-05-26 14:50:48 +0530

    add config

---
+++ src/config/aws_config.js
@@ -0,0 +1,16 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+const appConfig = {
+  apiGatewayInvokeUrl: "",
+  cognitoUserPoolId: "",
+  cognitoAppClientId: "",
+  cognitoIdentityPoolId: "",
+  appInstanceArn: "",
+  region: "", // Only supported region for Amazon Chime SDK Messaging as of this writing
+  attachments_s3_bucket_name: "",
+  accessKeyId: "",
+  secretAccessKey: "",
+  apiVersion: "",
+};
+export default appConfig;
